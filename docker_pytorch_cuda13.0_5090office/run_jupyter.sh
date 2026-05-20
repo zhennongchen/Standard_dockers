2ded3ff7503146e9779c868c7c0b470acb1cf5a8
Zhennong Chen <chenzhennong@gmail.com>
2026-05-20 14:20:35 +0800

    update

--- docker_pytorch_cuda13.0_5090office/run_jupyter.sh
+++ docker_pytorch_cuda13.0_5090office/run_jupyter.sh
@@ -8,7 +8,7 @@ WORKDIR="${SCRIPT_DIR}"
 
 # 新的标准命名
 CONTAINER="${CONTAINER_NAME:-pytorch_cuda130_container}"
-IMAGE="${DOCKER_IMAGE:-pytorch_cuda130:1.0}"
+IMAGE="${DOCKER_IMAGE:-pytorch_cuda13.0:2.0}"
 PORT="${PORT:-8888}"
 TOKEN="${JUPYTER_TOKEN:-mypw}"
 
